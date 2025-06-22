d9b4210daf9538a28029d7e586de498b31f88d7f
Johannes Haapanen <johannes.haapanen2@gmail.com>
2025-06-22 13:15:26 +0300

    1.1-1.7 done

---
+++ part1/course-information/src/Part1.jsx
@@ -0,0 +1,9 @@
+const Part1 = (props) => {
+  return (
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+  )
+}
+
+export default Part1
---
+++ part1/course-information/src/Part3.jsx
@@ -0,0 +1,10 @@
+const Part3 = (props) => {
+        console.log(props)
+  return (
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+  )
+}
+
+export default Part3
